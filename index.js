4f62d1225e4ad3cdd163ee3c77a9ca98218d9e2f
behaver <qianxing@yeah.net>
2015-09-13 16:23:59 +0800

    结构优化调整。

--- index.js
+++ index.js
@@ -1,128 +1,134 @@
 HTMLElement.prototype.bindWordsCatcher = function (methods) {
-	var container = this;
+  var container = this;
 
-	// 判断处理参数method
-	if (methods === undefined) {
-	 	methods = ["point", "select"];
-	} else if (typeof(methods) === 'string') {
-		methods = [methods];
-	} else if (methods.constructor !== Array) {
-    		throw new Error('The parem is illegal.');
-	}; 
+  // 判断处理参数method
+  if (methods === undefined) {
+    methods = ["point", "select"];
+  } else if (typeof(methods) === 'string') {
+    methods = [methods];
+  } else if (methods.constructor !== Array) {
+    throw new Error('The parem is illegal.');
+  }; 
 
-	while(methods.length) {
-		var method = methods.shift();
-		if (method === "point") {
-			//获取每个字符及其坐标;
-	    		function getWordPosition (container) {
-			      if (container.innerHTML !== undefined) {
-			      		var results = [];
-	        			// var position = [];//存放位置的数组;
-			        	var wandp ={};//存放字符的数组;
-			        	var content = container.childNodes;
-			       	// 遍历子节点
-			       	for (var i = 0; i < content.length; i++) {
-			       		var a = content[i];
-			            		// 遍历文本节点内字符
-			          		if (a.constructor == Text) {
-			            			var span = [];
-			            			for (var y = 0; y < a.length; y++) {
-				              		var newa = document.createElement("span");
-						              container.insertBefore(newa,a);
-						              newa.innerText = a.nodeValue[y];              
-						              span.push(newa);
-						              var position = getElementPosition(newa);
-						              var node = a.nodeValue[y];
-						              wandp.p = position;
-						              wandp.w = node;
-						           	results.push(wandp);
-			            			};
-			            			for (var j = 0; j < span.length; j++) {
-						              container.removeChild(span[j]);             
-					            	};
-			          		};
-		       		};
-			       };
-		      		return results;
-		      	};
-		      	
-		      	// 获取dom元素位置的函数;
-		    	function getElementPosition(element) {
-			       var oLeft = element.offsetLeft;
-			       var oTop = element.offsetTop;
-			       var oWidth = oLeft + element.offsetWidth;
-			       var oHeight = oTop + element.offsetHeight;    
-			       var oParent = element.offsetParent;
-			       while (oParent !== null) {
-			       	oLeft += oParent.offsetLeft;
-			       	oParent = oParent.offsetParent;
-		      		};  
-		      		return{x1: oLeft, x2: oWidth, y1: oTop, y2: oHeight};
-		    	};
+  while(methods.length) {
 
-		    	//获取鼠标位置;
-	    		function mousePosition (ev) {
-				var oEvent = ev || event;
-			       var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-			       var scrollLeft = document.documentElement.scrollLeft || document.body.scrollLeft;
-			       var xyArr = {x: oEvent.pageX, y: oEvent.pageY};
-			       return xyArr;
-	    		};
+    var method = methods.shift();
+    if (method === "point") {
+      //利用辅助span节点获取容器内每个字符及其坐标;
+      function getWordPosition (container) {
+        if (container.innerHTML !== undefined) {
+          var content = container.childNodes;
+          for (var i = 0; i < content.length; i++) {
+            var a = content[i];
+            if (a.constructor == Text) {
+              // 对文本节点的内容进行处理
+              var spans = [];
+              for (var j = 0; j < a.length; j++) {
+                // 创建用作辅助获取位置的span节点
+                var span = document.createElement("span");
+                container.insertBefore(span,a);
+                span.innerText = a.nodeValue[j];              
+                spans.push(span);
 
-		    	//输出鼠标所在区域字符;
-		    	function judge (ev) {
-		        	var xy = mousePosition(ev);
-			       var results = getWordPosition(container);
-			       for (var i = 0; i < results.length; i++) {
-			       	var p = results[i].p;
-			       	// console.log(results);
-			        	if (xy.x >= p.x1 && xy.x <= p.x2 && xy.y >= p.y1 && xy.y <= p.y2) { 
-					       alert(results[i].w);
-		        		};
-		      		};      
-		    	};
+                // 获取节点字位置信息并存入字典数组中
+                positions.push({word: a.nodeValue[j], position: span.getPosition()});
+              };
 
-		    	//定时器;
-		    	var timer,
-	       	positions = getWordPosition(container);
-		       container.onmousemove = function (e) {
-		       	clearTimeout(timer);
-		      		timer = setTimeout(function(e) {
-		        		return function () {
-		          			judge(e);
-		        		};
-		      		}(e), 1000);
-		    	};
-	        	container.onmouseout = function () {
-		       	clearTimeout(timer);
-		    	};
-		} else if (method === "select") {
-			//获取选中文字内容;
-		    	function getSelect() {
-			      	var selectTxt;
-			       if (window.getSelection) {//标准浏览器支持的方法
-		       		selectTxt = window.getSelection();
-			       };
-			      if (document.selection) {//IE浏览器支持的方法
-			       	selectTxt = document.selection.createRange().text;
-			       };
-			       return trim(selectTxt.toString());
-		    	};
+              // 清空所有插入的辅助节点，还原现场。
+              for (var j = 0; j < spans.length; j++) {
+                container.removeChild(spans[j]);             
+              };
+            };
+          };
+        };
+      };
 
-		    	//删除左右两端的空格;
-		    	function trim(str) { 
-		      		return str.replace(/(^\s*)|(\s*$)/g, "");
-		  　	};
+      //获取当前光标位置;
+      function getMousePosition (ev) {
+        var oEvent = ev || event;
+        var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+        var scrollLeft = document.documentElement.scrollLeft || document.body.scrollLeft;
+        var xyArr = {x: oEvent.pageX, y: oEvent.pageY};
+        return xyArr;
+      };
 
-		  	//鼠标选中事件;
-		    	container.onmouseup = function(e) {
-		      		var content = getSelect(e);
-		      		if (content != "" || null) {
-	        			explain.style.display = "block";
-		       	};  
-		    	};
-		} else {
-			throw new Error("error");
-		};
-	};
+      //输出鼠标所在区域字符;
+      function judge (ev) {
+        var xy = getMousePosition(ev);
+        for (var i = 0; i < positions.length; i++) {
+          if (xy.x >= positions[i].position.x1 && 
+              xy.x <= positions[i].position.x2 && 
+              xy.y >= positions[i].position.y1 && 
+              xy.y <= positions[i].position.y2) 
+          {  
+            console.log(positions[i].word);
+          };
+        };
+      };
+
+      document.body.addEventListener('load', function () {
+        // 全部字位置信息
+        var positions = [];
+        //定时器Handle;
+        var timer;
+        // 光标移入事件定义
+        container.onmousemove = function (e) {
+          clearTimeout(timer);
+          timer = setTimeout(function(e) {
+            return function () {
+              judge(e);
+            };
+          }(e), 1000);
+        };
+        // 光标移出事件定义
+        container.onmouseout = function () {
+          clearTimeout(timer);
+        };
+
+        // 扩展DOM获取位置方法;
+        HTMLElement.prototype.getPosition = function () {
+          var oLeft = this.offsetLeft;
+          var oTop = this.offsetTop;
+          var oWidth = oLeft + this.offsetWidth;
+          var oHeight = oTop + this.offsetHeight;    
+          var oParent = this.offsetParent;
+          while (oParent !== null) {
+            oLeft += oParent.offsetLeft;
+            oParent = oParent.offsetParent;
+          };  
+          return{x1: oLeft, x2: oWidth, y1: oTop, y2: oHeight};
+        };
+
+        
+      } ,false);
+    } else if (method === "select") {
+      //获取选中文字内容;
+      function getSelect() {
+        var selectTxt;
+        if (window.getSelection) {//标准浏览器支持的方法
+          selectTxt = window.getSelection();
+        };
+        if (document.selection) {//IE浏览器支持的方法
+          selectTxt = document.selection.createRange().text;
+        };
+        return trim(selectTxt.toString());
+      };
+
+      //删除左右两端的空格;
+      function trim(str) { 
+        return str.replace(/(^\s*)|(\s*$)/g, "");
+      };
+      document.body.addEventListener('load', function () {
+        //鼠标选中事件;
+        container.onmouseup = function(e) {
+          var content = getSelect(e);
+          if (content != "" || null) {
+            explain.style.display = "block";
+          };  
+        };
+      }, false);
+    } else {
+      throw new Error("error");
+    };
+  };
 };
